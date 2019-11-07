5049d9202d40d589bc4eb158a27319b04948a035
hearsay316 <908388349@qq.com>
2019-11-07 14:58:27 +0800

    1, 改变首页的bar 效果

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,62 @@
       <router-link to="/">首页</router-link>
       <router-link to="/course">我的课程</router-link>
       <router-link to="/profile">个人中心</router-link>
+      <cube-tab-bar
+        v-model="selectedLabelDefault"
+        :data="tabs"
+        @click="clickHandler"
+        @change="changeHandler"
+      >
+      </cube-tab-bar>
     </div>
     <router-view />
   </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selectedLabelDefault: "/",
+      tabs: [
+        {
+          label: "首页",
+          value: "/",
+          icon: "cubeic-home"
+        },
+        {
+          label: "我的课程",
+          value: "/course",
+          icon: "cubeic-like"
+        },
+        {
+          label: "个人中心",
+          value: "/profile",
+          icon: "cubeic-vip"
+        }
+      ]
+    };
+  },
+  watch: {
+    $route: {
+      handler(to, from) {
+        console.log(to, from);
+        this.selectedLabelDefault = to.path;
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    clickHandler(label) {
+      // if you clicked home tab, then print 'Home'
+      console.log(label);
+    },
+    changeHandler(label) {
+      // if you clicked different tab, this methods can be emitted
+      console.log(label);
+      this.$router.push(label);
+    }
+  }
+};
 </script>
 <style lang="stylus">
 #app
